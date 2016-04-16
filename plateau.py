6a616c5b5fb3cb42735ce068d7f4691a6bf7f1ca
Igor Tkanov <tkanoff@gmail.com>
2016-04-16 12:39:27 +0200

    More logic for Plateau and Rover classes

--- plateau.py
+++ plateau.py
@@ -18,6 +18,21 @@ class Plateau:
         self.upper_x = upper_x
         self.upper_y = upper_y
 
+    def can_move(self, current_x=None, current_y=None, x_add=None, y_add=None):
+        if current_x:
+            try:
+                return 0 <= (current_x + x_add) <= self.upper_x
+            except:
+                print "Cannot move from current X %s to %s" % (str(current_x), str(x_add))
+                return False
 
+        if current_y:
+            try:
+                return 0 <= (current_y + y_add) <= self.upper_y
+            except:
+                print "Cannot move from current Y %s to %s" % (str(current_y), str(y_add))
+                return False
 
+    def can_deploy_to(self, x, y):
+        return (0 <= x <= self.upper_x) and (0 <= y <= self.upper_y)
 
--- rover.py
+++ rover.py
@@ -1,12 +1,51 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 
 class Rover:
     position = None
     directions = None
+    plateau = None
+
+    position_x = None
+    position_y = None
+    position_facing = None
+
+    direction_transitions = {
+        'L': {
+            'N': 'W',
+            'W': 'S',
+            'S': 'E',
+            'E': 'N',
+        },
+        'R': {
+            'N': 'E',
+            'E': 'S',
+            'S': 'W',
+            'W': 'N',
+        }
+    }
 
     def __init__(self):
-        pass
+        self. map_transitions = {
+            'E': {
+                'steps': 1,
+                'update': 'x',
+            },
+            'N': {
+                'steps': 1,
+                'update': 'y',
+            },
+            'S': {
+                'steps': -1,
+                'update': 'y',
+            },
+            'W': {
+                'steps': -1,
+                'update': 'x',
+            },
+        }
 
     def set_directions(self, directions):
         self.directions = directions
@@ -14,9 +53,42 @@ class Rover:
     def set_position(self, position):
         self.position = position
 
+    def set_plateau(self, plateau):
+        self.plateau = plateau
+
     def is_ready(self):
         return self.position and self.directions
+
+    def deploy(self):
+        if self.plateau.can_deploy_to(self.position_x, self.position_y):
+            print "Getting to the position"
+        else:
+            print "Cannot move to the position"
+
+    def move(self):
+        for command in self.directions:
+            if command in ['L', 'R']:
+                self.position_facing = self.direction_transitions[command][self.position_facing]
+
+            if command == 'M':
+                # TODO: this can be made better by using self.* entities or some consts
+                update_coord = self.map_transitions[self.position_facing]['update']
+
+                if update_coord == 'x':
+                        self.position_x += self.map_transitions[self.position_facing]['steps']
+
+                if update_coord == 'y':
+                        self.position_y += self.map_transitions[self.position_facing]['steps']
+
+    def split_coordinates(self):
+        split_coords_pattern = re.compile("(\d{1}) (\d{1}) ([NESW]{1})")
+        coords = split_coords_pattern.match(self.position)
+
+        self.position_x = int(coords.group(1))
+        self.position_y = int(coords.group(2))
+        self.position_facing = coords.group(3)
 
     def __repr__(self):
         return '<%s object. Position: %s; Directions: %s>' \
                % (self.__class__.__name__, str(self.position), str(self.directions))
+
