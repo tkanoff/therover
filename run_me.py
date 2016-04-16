0c8a9d4376e69e3a21585131a6f07a19db1caf89
Igor Tkanov <tkanoff@gmail.com>
2016-04-16 11:15:54 +0200

    Run script

---
+++ run_me.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from plateau import Plateau
+from rover import Rover
+
+
+# Get test data from the file
+filename = "data.txt"
+
+rover_index = 0
+
+plateau_initial = {}
+rovers = {}
+
+rover_position_pattern = re.compile("(\d\ ){2}\w")
+directions_position_pattern = re.compile("^[A-Z]+")
+
+with open(filename) as f:
+    for line in f:
+        line = line.strip()
+        print line
+
+        # Get initial Plateau coordinates
+        if not plateau_initial:
+            try:
+                plateau_initial['x'], plateau_initial['y'] = line.split(' ')
+            except:
+                print('Failed to read Plateau initial coordinates')
+
+        print rovers.keys()
+        if rover_index not in rovers.keys():
+            rovers[rover_index] = Rover()
+
+        position = rover_position_pattern.match(line)
+        directions = directions_position_pattern.match(line)
+
+        if position:
+            rovers[rover_index].set_position(position.group(0))
+            print position.group(0)
+
+        if directions:
+            rovers[rover_index].set_directions(directions.group(0))
+            print directions.group(0)
+
+        if rovers[rover_index].is_ready():
+            rover_index += 1
+
+
+print plateau_initial
+print rovers
